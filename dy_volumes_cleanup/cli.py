5156ab4068790b73d7ec04d2f448f60b95144854
Dustin Kaiser <kaiser@itis.swiss>
2022-10-10 08:53:00 +0200

    print number of volumes before cleanup

--- dy_volumes_cleanup/cli.py
+++ dy_volumes_cleanup/cli.py
@@ -29,29 +29,32 @@ async def async_entrypoint(
     async with docker_client() as client:
         dyv_volumes: list[dict] = await get_dyv_volumes(client)
 
-        for dyv_volume in dyv_volumes:
-            volume_name = dyv_volume["Name"]
+        if len(dyv_volumes) > 0:
+            typer.echo(f"The dy-sidecar volume cleanup detected {len(dyv_volumes)} zombie volumes on the current machine.")
+            typer.echo(f"Beginning cleanup.")
+            for dyv_volume in dyv_volumes:
+                volume_name = dyv_volume["Name"]
 
-            if await is_volume_used(client, volume_name):
-                typer.echo(f"Skipped in use docker volume: '{volume_name}'")
-                continue
+                if await is_volume_used(client, volume_name):
+                    typer.echo(f"Skipped in use docker volume: '{volume_name}'")
+                    continue
 
-            await store_to_s3(
-                dyv_volume=dyv_volume,
-                s3_endpoint=s3_endpoint,
-                s3_access_key=s3_access_key,
-                s3_secret_key=s3_secret_key,
-                s3_bucket=s3_bucket,
-                s3_region=s3_region,
-                s3_provider=s3_provider,
-                s3_retries=s3_retries,
-                s3_parallelism=s3_parallelism,
-                exclude_files=exclude_files,
-            )
-            typer.echo(f"Pushed data to S3 for docker volume: '{volume_name}'")
+                await store_to_s3(
+                    dyv_volume=dyv_volume,
+                    s3_endpoint=s3_endpoint,
+                    s3_access_key=s3_access_key,
+                    s3_secret_key=s3_secret_key,
+                    s3_bucket=s3_bucket,
+                    s3_region=s3_region,
+                    s3_provider=s3_provider,
+                    s3_retries=s3_retries,
+                    s3_parallelism=s3_parallelism,
+                    exclude_files=exclude_files,
+                )
+                typer.echo(f"Succesfully pushed data to S3 for zombie dynamic sidecar docker volume: '{volume_name}'")
 
-            await delete_volume(client, volume_name)
-            typer.echo(f"Removed docker volume: '{volume_name}'")
+                await delete_volume(client, volume_name)
+                typer.echo(f"Removed docker volume: '{volume_name}'")
 
 
 app = typer.Typer()
